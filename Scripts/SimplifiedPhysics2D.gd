d818ef1066a8b7eec91bbe51d2aace64d731717b
Bruno Guedes <guedesav@gmail.com>
2019-11-27 00:30:29 -0200

    Physics Revamp
    - Add bounce and pushback features
    - Refactor floor and collision check to use move_and_slide
    return values.

--- Scripts/SimplifiedPhysics2D.gd
+++ Scripts/SimplifiedPhysics2D.gd
@@ -18,11 +18,15 @@ var hspeed:float
 var friction:float
 var gravity:float
 
+var debug_velocity:Vector2
+
 var onFloor:bool = false
 var onAir:bool = false
 
 signal floored
 signal airborne
+signal bounce
+signal pushback
 
 func _ready():
     friction = walkSpeed / dampTime
@@ -31,9 +35,13 @@ func _ready():
     pass # Replace with function body.
 
 func _process(dt):
-    move_and_slide(Vector2(hspeed, vspeed))
-   
-    check_floor()
+    var velocity = move_and_slide(Vector2(hspeed, vspeed), Vector2(0, -1))
+    debug_velocity = velocity
+
+    if velocity.y == 0:
+        check_floor()
+    
+    check_bounce()
     
     if onFloor:
         if hspeed > 0:
@@ -44,14 +52,14 @@ func _process(dt):
     else:
         vspeed += gravity*dt
 
-func jump():
-    if onFloor:
-        var jumpSpeed:float = sqrt(2 * gravity * jumpHeight)
+func jump(height := 1.0, force := false):
+    if onFloor or force:
+        var jumpSpeed:float = sqrt(2 * gravity * height * jumpHeight)
         vspeed = -jumpSpeed
         onAir = true
         onFloor = false
         emit_signal("airborne")
-    
+
 func walk(dir):
     var bump:KinematicCollision2D = move_and_collide(Vector2(dir, 0).normalized(), true, true, true)
     
@@ -66,13 +74,31 @@ func check_floor():
     
     var touchedDown:bool = bump != null
     
-    if not onFloor and vspeed >= 0 and touchedDown:
+    if not onFloor and touchedDown:
         emit_signal("floored")
-        
         onFloor = true
         onAir = onAir and not touchedDown
     if onFloor and not touchedDown:
         emit_signal("airborne")
         onAir = true
         onFloor = false
-    
+
+func check_bounce():
+    for i in get_slide_count():
+        var collision:KinematicCollision2D = get_slide_collision(i)
+        if collision.normal.y < 0:
+            if collision.position.y >= position.y:
+                if "bounce" in collision.collider:
+                    jump(collision.collider.bounce, true) 
+                    emit_signal("bounce", collision.collider)
+                    return true
+        if abs(collision.normal.x) > 0.8:
+            if "pushback" in collision.collider:
+                jump(0.25, true)
+                hspeed = collision.normal.x * collision.collider.pushback
+                return true
+            
+    return false
+
+func _draw():
+    draw_line(position, position + debug_velocity, Color.maroon, 2.0, true)
